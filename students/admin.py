a4953a55f6d07ab352ef60c69b3106b1115f8b1a
ImranDesktop <imranthelegend12@gmail.com>
2024-10-04 20:11:54 +0600

    blood group, last donated and phone number field added in model. now next step is showing them in profile page

--- students/admin.py
+++ students/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Student_Model, Certificate_Photo, Thumbnail_photo
+from .forms import Update_student_profile
 
 # Register your models here.
-admin.site.register(Student_Model)
+class Student_admin(admin.ModelAdmin):
+    form = Update_student_profile
+
+admin.site.register(Student_Model, Student_admin)
 admin.site.register(Certificate_Photo)
 admin.site.register(Thumbnail_photo)
